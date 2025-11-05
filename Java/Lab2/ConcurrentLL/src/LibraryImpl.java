e1544bede52f48502037b84c6ee69e18e21050be
babadzakich <doctorare@mail.ru>
2025-11-05 10:21:24 +0700

    fixes

--- Java/Lab2/ConcurrentLL/src/LibraryImpl.java
+++ Java/Lab2/ConcurrentLL/src/LibraryImpl.java
@@ -2,6 +2,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -61,7 +62,7 @@ public class LibraryImpl {
         boolean swap = false;
         try {
             while (run.get()) {
-                Thread.sleep(5000);
+                Thread.sleep(Duration.ofSeconds(5));
                 boolean flag = true;
                 while (flag) {
                     int i = 0;
@@ -75,12 +76,13 @@ public class LibraryImpl {
                                 swap = true;
                                 flag = true;
                                 Collections.swap(list, i, i + 1);
+                                stepCount.incrementAndGet();
                             }
                         }
                         if (done) break;
-                        stepCount.incrementAndGet();
+
                         if (swap) {
-                            Thread.sleep(1000);
+                            Thread.sleep(Duration.ofSeconds(1));
                         }
                         i++;
                     }
